71a49e3d39403fe0b34b0e3e7c51d5fd5b288c12
techlateef <techlateef@gmail.com>
2026-04-01 14:05:31 +0100

    fix name correction

--- submission/02.sh
+++ submission/02.sh
@@ -1,3 +1,3 @@
 # Create a native segwit address and get the public key from the address.
-bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress "" bech32
-bitcoin-cli -regtest -rpcwallet=builderswallet getaddressinfo <address> | jq -r '.pubkey'
+address=$(bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress "" bech32)
+bitcoin-cli -regtest -rpcwallet=builderswallet getaddressinfo $address | jq -r '.pubkey'
